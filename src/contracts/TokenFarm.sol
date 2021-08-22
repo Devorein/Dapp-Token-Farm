15afb8a1f11f6d8ce84b10722f6bee62028c69e7
Safwan Shaheer <devorein00@gmail.com>
2021-08-22 22:29:50 +0600

    Created basic TokenFarm bank

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -2,6 +2,16 @@
 
 pragma solidity ^0.8.7;
 
+import './DaiToken.sol';
+import './DappToken.sol';
+
 contract TokenFarm {
     string public name = 'Dapp Token Farm';
+    DaiToken public daiToken;
+    DappToken public dappToken;
+
+    constructor(DaiToken _daiToken, DappToken _dappToken) {
+        daiToken = _daiToken;
+        dappToken = _dappToken;
+    }
 }
