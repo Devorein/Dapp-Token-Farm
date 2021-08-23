7e035cab36f6efa5f63d85b0160892e1a20ac63f
Safwan Shaheer <devorein00@gmail.com>
2021-08-23 15:53:47 +0600

    Added function to stake dai token in Token Farm smart contract

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -10,8 +10,34 @@ contract TokenFarm {
     DaiToken public daiToken;
     DappToken public dappToken;
 
+    // An array of all the stakers of dai token
+    address[] public stakers;
+    // Keeps track of the staked balance of the investor
+    mapping(address => uint256) public stakingBalance;
+    // Keeps track of all the investors that have staked
+    mapping(address => bool) public hasStaked;
+    // Keeps track of all the investors current staking status
+    mapping(address => bool) public isStaking;
+
     constructor(DaiToken _daiToken, DappToken _dappToken) {
         daiToken = _daiToken;
         dappToken = _dappToken;
+    }
+
+    // Transfer dai token from investors wallet to the smart contracts
+    function stakeTokens(uint256 _amount) public {
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+
+        // Update the investors staked amount
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        // Add investor to the stakers array if they haven't staked already
+        if (!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+
+        // Update staking status of the current investor
+        hasStaked[msg.sender] = true;
+        isStaking[msg.sender] = true;
     }
 }
